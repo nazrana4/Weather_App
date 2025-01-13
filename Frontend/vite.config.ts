63bb985f232c118004f81edab1a295808e116f86
nazrana/4 <nazranabagwan04@gmail.com>
2025-01-14 03:24:00 +0530

    last commit

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Weather_App/Frontend/',
+  base: '/Weather_App/',
   plugins: [react()],
 })
